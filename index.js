7a06409d7a55d10ba3b3af672dc1c6001643d46c
cynthia <cynthiakagz@gmail.com>
2023-03-30 21:19:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const feetPerBlock=264; 
+
+function distanceFromHqInBlocks(distance) { 
+    
+       return Math.abs(distance-42)
+       
+      
+}
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block)*feetPerBlock
+}
+function distanceTravelledInFeet(startBlock,destinationBlock) {
+ return Math.abs((startBlock-destinationBlock)*feetPerBlock)
+}
+function calculatesFarePrice(startBlock,destinationBlock) {
+  const distanceInFeet=distanceTravelledInFeet(startBlock,destinationBlock)
+  if(distanceInFeet<=400) {return 0;}
+  else if(distanceInFeet<2000) {return (distanceInFeet-400)*0.02;}
+  else if(distanceInFeet<2500) {return 25;}
+  else{return "cannot travel that far";}
+}
+   
